d07eb1bd3754f0e949513bd69d5cf4cd1dc3fd4f
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-03-23 21:07:02 +0300

    Renamed Lessons 01...09, added Lessons 23-24)

---
+++ src/Lesson23/Lesson23Part2.java
@@ -0,0 +1,15 @@
+package Lesson23;
+
+public class Lesson23Part2 {
+    public static void main(String []args){
+        /*System.out.print("Hello");
+        System.out.println("Hello again");
+        System.out.printf("This is a string, %s", "and this is nice!");*/
+        System.out.printf("String %-5d \n", 532);
+        System.out.printf("String %-5d \n", 5);
+        System.out.printf("String %-5d \n", 33);
+        System.out.printf("String %-5d \n", 1000000000);
+        System.out.printf("String %.4f \n", 532.75648418);
+        System.out.printf("String %.4f \n", 1735.7);
+    }
+}
