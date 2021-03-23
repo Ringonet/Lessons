e454db95363d2baffd6172e1ea6c520647ad7f01
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-03-23 21:44:43 +0300

    Changed Lesson 24

--- src/Lesson24/Lesson24.java
+++ src/Lesson24/Lesson24.java
@@ -1,4 +1,24 @@
 package Lesson24;
 
 public class Lesson24 {
+    public static void main(String []args){
+        Human human1 = new Human("Artem",33);
+        human1.toString(); // toString is a hidden method
+        System.out.println(human1);
+    }
+}
+
+class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString(){
+        return name+", "+age;
+    }
+
 }
