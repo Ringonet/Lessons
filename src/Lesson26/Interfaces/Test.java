17dc35c9354a566979afeef10bbab84729cd9137
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-03-24 21:40:31 +0300

    Added Lessons 26-27

---
+++ src/Lesson26/Interfaces/Test.java
@@ -0,0 +1,22 @@
+package Lesson26.Interfaces;
+
+public class Test {
+    public static void main(String []args){
+        Animal animal1 = new Animal(002);
+        Person person1 = new Person("Artem Bogdanov");
+        //animal1.sleep();
+        //person1.sayHello();
+        //animal1.showInfo();
+        //person1.showInfo();
+        //Info info1 = new Animal(1);
+        //Info info2 = new Person("Artem");
+        //info1.showInfo();
+        //info2.showInfo();
+        outputInfo(animal1);
+        outputInfo(person1);
+    }
+
+    public static void outputInfo(Info info){
+        info.showInfo();
+    }
+}
---
+++ src/Lesson27/Test.java
@@ -0,0 +1,19 @@
+package Lesson27;
+
+//import Lesson27.Forest.Something.SomeClass;
+//import Lesson27.Forest.Squirell;
+//import Lesson27.Forest.Tree;
+import java.util.Scanner;
+import Lesson27.Forest.*;
+import Lesson27.Forest.Something.*;
+
+
+public class Test {
+    public static void main(String []args){
+        Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();
+        Scanner scanner1 = new Scanner(System.in);
+        SomeClass someClass1 = new SomeClass();
+
+    }
+}
