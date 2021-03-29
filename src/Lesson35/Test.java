d3f63d7c6194a0d2b949cc73479a157218d758c3
Artem Bogdanov <bogdanov-a-s2014@yandex.ru>
2021-03-29 22:15:27 +0300

    Added Lesson 35

---
+++ src/Lesson35/Test.java
@@ -0,0 +1,13 @@
+package Lesson35;
+
+class Animal {
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
